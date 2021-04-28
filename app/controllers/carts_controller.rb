dbbf39fd2d35bcaba474b81b1b27cd5d035595b2
Nicoll Oliver <nicolldon96@gmail.com>
2021-04-27 22:17:26 -0400

    Cart Products Controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,51 @@
 class CartsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+    before_action :set_cart, only: [:show, :edit, :update, :destroy]
+    def new
+        @cart = Cart.new
+    end
+
+
+    def create
+        @cart = Cart.new(cart_params)
+
+        if @cart.save 
+            redirect_to cart_path(@cart)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @cart = Cart.all
+    end
+
+    def show
+        @cart = Cart.find(params[:id])
+    end
+
+    def edit 
+
+    end
+
+    def destroy
+        @cart.destroy if @cart.id === session[:cart_id]
+        session[:cart_id] = nil
+
+        redirect_to root_path
+    end
+
+    def checkout
+
+    end
+
+    private 
+    def cart_params
+        params.require(:cart).permit(:user_id, :checkout)
+    end
+
+    def invalid_cart
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to root_path, notice: "That cart doesn't exist"
+    end
 end
