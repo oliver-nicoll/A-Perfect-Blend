bd18d18e792486e5916f5f35c9704d0ef77028ad
Nicoll Oliver <nicolldon96@gmail.com>
2021-04-27 22:12:37 -0400

    User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+      def profile
+        render 'edit'
+      end
+  
+      def vendor
+      end
+  
+      def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:message] = "Successfully signed up."
+          session[:user_id] = @user.id
+          redirect_to products_path
+        else
+          render :new
+        end
+      end
+    
+      def destroy
+        session.delete("user_id")
+        redirect_to root_path
+      end
+  
+      private
+  
+      def user_params
+        params.require(:user).permit(:vendor, :name, :username, :password, :email)
+      end
 end
